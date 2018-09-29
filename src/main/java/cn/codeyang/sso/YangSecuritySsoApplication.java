f2ff0a2f31f0587031d9d0eb90ca3ca48e20c87e
yangzhongyang <306525121@qq.com>
2018-09-29 10:49:36 +0800

    first

---
+++ src/main/java/cn/codeyang/sso/YangSecuritySsoApplication.java
@@ -0,0 +1,12 @@
+package cn.codeyang.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangSecuritySsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangSecuritySsoApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/codeyang/sso/YangSecuritySsoApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.codeyang.sso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YangSecuritySsoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
